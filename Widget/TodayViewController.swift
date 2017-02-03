018a934e2a0544297790519e73d5eaf31a3c1854
Charles Renwick <charles.srenwick@gmail.com>
2017-02-03 18:49:19 -0500

    widget init

---
+++ Widget/TodayViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TodayViewController.swift
+//  Widget
+//
+//  Created by Charles Renwick on 2/3/17.
+//  Copyright © 2017 Charles Renwick. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(.newData)
+    }
+    
+}
