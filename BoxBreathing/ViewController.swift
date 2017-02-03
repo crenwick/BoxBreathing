9a4401b54d208daea9a04b3de62f455a5658e16b
Charles Renwick <charles.srenwick@gmail.com>
2017-02-03 16:24:39 -0500

    xcode init

---
+++ BoxBreathing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoxBreathing
+//
+//  Created by Charles Renwick on 2/3/17.
+//  Copyright © 2017 Charles Renwick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
